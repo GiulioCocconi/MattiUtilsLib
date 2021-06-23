092e23e3722d70e27bd3a95cad244bd236dafdb2
Giulio Cocconi <31406038+GiulioCocconi@users.noreply.github.com>
2021-06-23 22:08:22 +0200

    Create MattiUtilsLib.h

---
+++ include/MattiUtilsLib.h
@@ -0,0 +1,13 @@
+#ifndef MY_LIBRARY_H
+#define MY_LIBRARY_H
+
+#include <Arduino.h>
+
+String getValue(String, char, int);
+
+double mapf(double, double, double, double, double);
+
+char* beautifyStr(String);
+
+
+#endif
